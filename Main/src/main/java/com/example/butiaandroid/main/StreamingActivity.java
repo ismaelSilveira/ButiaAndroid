f50d4950da27f2b00719640a9f2390dbb86074a6
rodrigo <ro.goab@gmail.com>
2014-05-01 22:19:34 -0300

    consume gstream

--- Main/src/main/java/com/example/butiaandroid/main/StreamingActivity.java
+++ Main/src/main/java/com/example/butiaandroid/main/StreamingActivity.java
@@ -1,7 +1,9 @@
 package com.example.butiaandroid.main;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -10,16 +12,33 @@ import android.widget.ImageView;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
+import com.example.butiaandroid.main.stream.MjpegInputStream;
+import com.example.butiaandroid.main.stream.MjpegView;
 import com.example.butiaandroid.main.vistas.LayoutControl;
+
+import java.net.Socket;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
 
 
 public class StreamingActivity extends Activity {
 
-    VideoView videoView;
+  //  VideoView videoView;
     LayoutControl control;
     ImageView robot;
     int velMAX = 1000;
     Robot butia;
+
+    private MjpegView mv = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +47,7 @@ public class StreamingActivity extends Activity {
 
         control = (LayoutControl) findViewById(R.id.control);
         robot = (ImageView) findViewById( R.id.robot);
-        videoView = (VideoView)this.findViewById(R.id.videoView);
+//        videoView = (VideoView)this.findViewById(R.id.videoView);
 
         control.setTransparente(true);
 
@@ -39,6 +58,7 @@ public class StreamingActivity extends Activity {
         Control c = new Control( control, robot);
         control.setOnTouchListener(c);
 
+        /*con vlc era solo usar un mediaplayer
         //add controls to a MediaPlayer like play, pause.
         MediaController mc = new MediaController(this);
         videoView.setMediaController(mc);
@@ -52,12 +72,92 @@ public class StreamingActivity extends Activity {
         //Set the focus
         videoView.requestFocus();
         videoView.start();
+        */
 
 
+//a probar
+/*        String KEY_HOSTNAME = "hostname";
+        String KEY_PORTNUM = "portnum";
+        String KEY_WIDTH = "width";
+        String KEY_HEIGHT = "height";
+        // store the input data
+        SharedPreferences sp = this.getPreferences( MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putString( KEY_HOSTNAME, hostname);
+        editor.putString( KEY_PORTNUM, portnum);
+        editor.putString( KEY_WIDTH, width);
+        editor.putString( KEY_HEIGHT, height);
+        editor.commit();
+*/
+
+//hasta acaaaaaaaaaa
+
+        int width = 800;
+        int height = 600;
+
+        // set the image size
+        MjpegView.setImageSize( width, height);
+
+        mv = (MjpegView) findViewById(R.id.mv);
+        String hostname ="172.16.101.128";
+        String portnum ="1234";
+        new DoRead().execute( hostname, portnum);
     }
 
 
+    public void onResume() {
+        if(mv!=null){
+            mv.resumePlayback();
+        }
 
+    }
+
+    public void onStart() {
+        super.onStart();
+    }
+
+    public void onPause() {
+        super.onPause();
+        if(mv!=null){
+            mv.stopPlayback();
+        }
+    }
+
+    public void onStop() {
+        super.onStop();
+    }
+
+    public void onDestroy() {
+        if(mv!=null){
+            mv.freeCameraMemory();
+        }
+
+        super.onDestroy();
+    }
+
+    public class DoRead extends AsyncTask<String, Void, MjpegInputStream> {
+        protected MjpegInputStream doInBackground( String... params){
+            Socket socket = null;
+            try {
+                socket = new Socket( params[0], Integer.valueOf( params[1]));
+                return (new MjpegInputStream(socket.getInputStream()));
+            } catch (UnknownHostException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        protected void onPostExecute(MjpegInputStream result) {
+            mv.setSource(result);
+            if(result!=null) result.setSkip(1);
+            mv.setDisplayMode(MjpegView.SIZE_BEST_FIT);
+            mv.showFps(true);
+        }
+    }
 
 
 }
